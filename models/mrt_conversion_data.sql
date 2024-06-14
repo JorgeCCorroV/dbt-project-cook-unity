e5569b107fae40a56cc676a0ee0fa41f6c72feb2
Jorge Corro <jorgeccv_2@hotmail.com>
2024-06-14 20:41:11 +0000

    refactor mrt model

--- models/mrt_conversion_data.sql
+++ models/mrt_conversion_data.sql
@@ -1,3 +1,14 @@
+{% set columns_to_sum =
+    [
+        'INTEREST', 
+        'IMPRESSIONS',
+        'CLICKS',
+        'SPENT',
+        'TOTAL_CONVERSION',
+        'APPROVED_CONVERSION'
+    ]
+%}
+
 with conversion_metrics as (
     
     select 
@@ -5,29 +16,18 @@ with conversion_metrics as (
         AD_ID,
         XYZ_CAMPAIGN_ID,
         AGE,
-        GENDER,
-        sum(INTEREST) as total_interest,
-        sum(IMPRESSIONS) as total_impressions,
-        sum(CLICKS) as total_clicks,
-        sum(SPENT) as total_spent,
-        sum(TOTAL_CONVERSION) as total_conversions,
-        sum(APPROVED_CONVERSION) as total_approved_conversion,
+        GENDER
 
-        {{ dbt_utils.safe_divide('total_clicks', 'total_impressions') }} * 100 as ctr,
+        {%- for columns in columns_to_sum %}
+        
+            ,coalesce(
+                sum({{ columns }}),
+                0
+            ) as total_{{ columns }}
+            
+        {% endfor %}
 
-        {{ dbt_utils.safe_divide('total_spent', 'total_clicks') }} * 100 as cpc,
 
-        {{ dbt_utils.safe_divide('total_spent', 'total_impressions') }} * 100 as cpm,
-
-        {{ dbt_utils.safe_divide('total_spent', 'total_conversions') }} * 100 as cpa,
-
-        {{ dbt_utils.safe_divide('total_conversions', 'total_clicks') }} * 100 as conversion_rate_clicks,
-
-        {{ dbt_utils.safe_divide('total_approved_conversion', 'total_clicks') }} * 100 as approved_conversion_rate_clicks,
-
-        {{ dbt_utils.safe_divide('total_approved_conversion', 'total_conversions') }} * 100 as approved_conversion_rate_orders,
-
-        {{ dbt_utils.safe_divide('total_spent', 'total_approved_conversion') }} * 100 as cost_per_approved_conversion
         
     from {{ ref('cook_unity_conversion_data') }}
     {{ dbt_utils.group_by(n=4) }}

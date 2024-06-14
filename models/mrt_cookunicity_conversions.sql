a4a531bb3126aff73fe24862cc26f977990909a9
Jorge Corro <jorgeccv_2@hotmail.com>
2024-06-14 20:58:52 +0000

    refactor mrt model

--- models/mrt_cookunicity_conversions.sql
+++ models/mrt_cookunicity_conversions.sql
@@ -9,6 +9,17 @@
     ]
 %}
 
+{%- set conversion_metrics = [
+    {'name': 'ctr', 'numerator': 'total_clicks', 'denominator': 'total_impressions'},
+    {'name': 'cpc', 'numerator': 'total_spent', 'denominator': 'total_clicks'},
+    {'name': 'cpm', 'numerator': 'total_spent', 'denominator': 'total_impressions'},
+    {'name': 'cpa', 'numerator': 'total_spent', 'denominator': 'total_conversions'},
+    {'name': 'conversion_rate_clicks', 'numerator': 'total_conversions', 'denominator': 'total_clicks'},
+    {'name': 'approved_conversion_rate_clicks', 'numerator': 'total_approved_conversions', 'denominator': 'total_clicks'},
+    {'name': 'approved_conversion_rate_orders', 'numerator': 'total_approved_conversions', 'denominator': 'total_conversions'},
+    {'name': 'cost_per_approved_conversion', 'numerator': 'total_spent', 'denominator': 'total_approved_conversions'}
+] -%}
+
 with conversion_metrics as (
     
     select 
@@ -16,25 +27,22 @@ with conversion_metrics as (
         ad_id,
         xyz_campaign_id,
         age,
-        gender,
+        gender
 
         {%- for columns in columns_to_sum %}
         
-            coalesce(
+            ,coalesce(
                 sum({{ columns }}),
                 0
-            ) as total_{{ columns }},
+            ) as total_{{ columns }}
             
         {% endfor %}
 
-        {{ calculate_metric('total_clicks', 'total_impressions') }} as ctr,
-        {{ calculate_metric('total_spent', 'total_clicks') }} as cpc,
-        {{ calculate_metric('total_spent', 'total_impressions') }} as cpm,
-        {{ calculate_metric('total_spent', 'total_conversions') }} as cpa,
-        {{ calculate_metric('total_conversions', 'total_clicks') }} as conversion_rate_clicks,
-        {{ calculate_metric('total_approved_conversions', 'total_clicks') }} as approved_conversion_rate_clicks,
-        {{ calculate_metric('total_approved_conversions', 'total_conversions') }} as approved_conversion_rate_orders,
-        {{ calculate_metric('total_spent', 'total_approved_conversions') }} as cost_per_approved_conversion
+        {% for metric in conversion_metrics %}
+            
+            ,{{ calculate_metric(metric.numerator, metric.denominator) }} as {{ metric.name }}
+
+        {% endfor %}
 
         
     from {{ ref('stg_cookunicity_conversions') }}
